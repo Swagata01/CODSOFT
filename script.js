e18d3a888a7651bc7832ca0aa3a0ff6692fc53ed
Swagata Dey <105094071+Swagata01@users.noreply.github.com>
2023-10-06 12:53:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript code for handling dynamic content (e.g., quiz creation, taking, and results)
+
+// Sample function to load a quiz creation form
+function loadCreateQuizForm() {
+    // Fetch and display the quiz creation form
+}
+
+// Sample function to load a quiz
+function loadQuiz(quizId) {
+    // Fetch quiz questions and display them one at a time
+}
+
+// Sample function to submit quiz answers and show results
+function submitQuizAnswers(answers) {
+    // Calculate the score and display the results
+}
